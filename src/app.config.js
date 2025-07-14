7bb4a7f7351206e417c366d68da1d26e223741b8
Ahmad Barclay <Ahmad.Barclay@ons.gov.uk>
2025-07-14 16:43:52 +0100

    fixed base paths

--- src/app.config.js
+++ src/app.config.js
@@ -1,6 +1,6 @@
 // APP CONFIG
 
 // Base paths
-export const base_prod = '/visualisations/sveltekit-starter'; // Directory on the ONS website
-export const base_preview = '/sveltekit-starter'; // Directory on datavisweb preview server or Github Pages
+export const base_prod = ''; // Directory on the ONS website
+export const base_preview = ''; // Directory on datavisweb preview server or Github Pages
 
