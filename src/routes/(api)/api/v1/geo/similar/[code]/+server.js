b99b01a133ee564c23c27444867e463e70fe7e9a
Ahmad Barclay <Ahmad.Barclay@ons.gov.uk>
2025-09-24 14:43:03 +0100

    add similar areas endpoint

---
+++ src/routes/(api)/api/v1/geo/similar/[code]/+server.js
@@ -0,0 +1,32 @@
+import { json, error } from "@sveltejs/kit";
+import geoGroups from "$lib/geo-groups.js";
+import areasClusters from "$lib/areas-clusters.json";
+import areasSimilar from "$lib/areas-similar.json";
+
+export function GET({ params }) {
+  const code = params.code;
+  const similar = areasSimilar[code];
+  if (!similar) return error(400, "Similar areas not available for selected code");
+
+  const isLTLA = geoGroups.ltla.codes.includes(code.slice(0, 3));
+
+  const clusters = [];
+  const clusterKeys = Object.keys(similar);
+  const clusterIndex = areasClusters.data.areacd.indexOf(code);
+  for (const key of clusterKeys) {
+    const obj = {
+      key,
+      label: key[0].toUpperCase() + key.slice(1),
+      similar: similar[key]
+    };
+    if (isLTLA) {
+      obj.cluster = {key: areasClusters.data[key][clusterIndex]};
+      obj.cluster.label = obj.cluster.key.toUpperCase();
+      obj.cluster.areas = areasClusters.data.areacd.filter((cd, i) => areasClusters.data[key][i] === obj.cluster.key);
+      obj.cluster.description = areasClusters.descriptions.find(d => d.type === key && d.letter === obj.cluster.key)?.text;
+    }
+    clusters.push(obj);
+  }
+
+  return json(clusters);
+}
