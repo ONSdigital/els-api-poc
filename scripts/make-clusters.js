b8d11f767c9a64279341cff66a9dde6654f48f34
Ahmad Barclay <Ahmad.Barclay@ons.gov.uk>
2025-09-25 14:23:46 +0100

    update similar areas data format

--- scripts/make-clusters.js
+++ scripts/make-clusters.js
@@ -6,10 +6,37 @@ const output_similar = "./src/lib/areas-similar.json";
 
 const config = JSON.parse(readFileSync(config_path));
 
-const clusters = config.clustersLookup;
-writeFileSync(output_clusters, JSON.stringify(clusters));
-console.log(`Wrote ${output_clusters}`);
-
 const similar = config.neighbourLookup;
 writeFileSync(output_similar, JSON.stringify(similar));
 console.log(`Wrote ${output_similar}`);
+
+const rawClusters = config.clustersLookup;
+const clusters = {
+  types: rawClusters.types,
+  keys: {},
+  lookup: {},
+  clusters: {},
+  descriptions: {}
+};
+
+for (const type of clusters.types) {
+  clusters.clusters[type] = {};
+  clusters.descriptions[type] = {};
+  const keys = rawClusters.descriptions.filter(d => d.type === type).map(d => d.letter);
+  clusters.keys[type] = keys;
+
+  for (const key of keys) {
+    clusters.descriptions[type][key] = rawClusters.descriptions
+      .filter(d => d.type === type && d.letter === key)
+      .map(d => d.text);
+    clusters.clusters[type][key] = rawClusters.data.areacd.filter((d, i) => rawClusters.data[type][i] === key);
+
+    for (const cd of clusters.clusters[type][key]) {
+      if (!clusters.lookup[cd]) clusters.lookup[cd] = {};
+      clusters.lookup[cd][type] = key;
+    }
+  }
+}
+
+writeFileSync(output_clusters, JSON.stringify(clusters));
+console.log(`Wrote ${output_clusters}`);
--- src/routes/(api)/api/v1/geo/similar/[code]/+server.js
+++ src/routes/(api)/api/v1/geo/similar/[code]/+server.js
@@ -1,32 +1,40 @@
 import { json, error } from "@sveltejs/kit";
-import geoGroups from "$lib/geo-groups.js";
+// import geoGroups from "$lib/geo-groups.js";
 import areasClusters from "$lib/areas-clusters.json";
 import areasSimilar from "$lib/areas-similar.json";
 
-export function GET({ params }) {
-  const code = params.code;
+function getSimilarAreas(code) {
   const similar = areasSimilar[code];
-  if (!similar) return error(400, "Similar areas not available for selected code");
-
-  const isLTLA = geoGroups.ltla.codes.includes(code.slice(0, 3));
+  if (!similar) return null;
 
   const clusters = [];
-  const clusterKeys = Object.keys(similar);
-  const clusterIndex = areasClusters.data.areacd.indexOf(code);
-  for (const key of clusterKeys) {
+  const clusterTypes = areasClusters.types;
+  const clusterLookup = areasClusters.lookup[code];
+  
+  for (const type of clusterTypes) {
     const obj = {
-      key,
-      label: `${key === "global" ? "All" : key[0].toUpperCase() + key.slice(1)} indicators`,
-      similar: similar[key]
+      key: type,
+      label: `${type === "global" ? "All" : type[0].toUpperCase() + type.slice(1)} indicators`,
+      similar: similar[type]
     };
-    if (isLTLA) {
-      obj.cluster = {key: areasClusters.data[key][clusterIndex]};
-      obj.cluster.label = obj.cluster.key.toUpperCase();
-      obj.cluster.areas = areasClusters.data.areacd.filter((cd, i) => areasClusters.data[key][i] === obj.cluster.key);
-      obj.cluster.description = areasClusters.descriptions.find(d => d.type === key && d.letter === obj.cluster.key)?.text;
+    
+    if (clusterLookup) {
+      const key = clusterLookup[type];
+      obj.cluster = {key};
+      obj.cluster.label = key.toUpperCase();
+      obj.cluster.areas = areasClusters.clusters[type][key];
+      obj.cluster.description = areasClusters.descriptions[type][key];
     }
     clusters.push(obj);
   }
+  return clusters;
+}
 
-  return json(clusters);
+export function GET({ params }) {
+  const code = params.code;
+
+  const similarAreas = getSimilarAreas(code);
+  if (!similarAreas) return error(400, `Similar areas not available for ${code}`);
+
+  return json(similarAreas);
 }
