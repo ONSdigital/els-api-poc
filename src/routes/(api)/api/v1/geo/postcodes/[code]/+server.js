a534abecd357b42c203be8963a942b66dfd1c6bd
Ahmad Barclay <Ahmad.Barclay@ons.gov.uk>
2025-09-30 12:28:07 +0100

    add error handler

--- src/routes/(api)/api/v1/geo/postcodes/[code]/+server.js
+++ src/routes/(api)/api/v1/geo/postcodes/[code]/+server.js
@@ -5,15 +5,14 @@ import { isValidPostcode } from "$lib/api/utils.js";
 
 export async function GET({ params }) {
   const code = params.code;
-  if (!isValidPostcode(code.toUpperCase())) error(400, `${code} is not a valid postcode`);
+  if (!isValidPostcode(code.toUpperCase()))
+    error(400, `${code} is not a valid postcode`);
 
   const postcode = await getPostcode(code);
   if (postcode.error) error(postcode.error, postcode.message);
 
-  const areas = {
-    ...postcode,
-    areas: await getAreasByPostcode(postcode)
-  };
+  const areas = await getAreasByPostcode(postcode);
+  if (areas.error) error(areas.error, areas.message);
 
-  return json(areas);
+  return json({ ...postcode, areas });
 }
