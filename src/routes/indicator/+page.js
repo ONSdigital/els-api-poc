f0715a80f316f3fdd80a4d4b5b7ff81cfc1033fc
Ahmad Barclay <Ahmad.Barclay@ons.gov.uk>
2025-10-08 16:23:56 +0100

    fixed single indicator demo

---
+++ src/routes/indicator/+page.js
@@ -0,0 +1,15 @@
+import { resolve } from '$app/paths';
+
+export const prerender = false;
+export const trailingSlash = 'always';
+
+export const load = async ({ fetch }) => {
+  const exclude = ["population-by-age-and-sex"];
+
+	const path = resolve(`/api/v1/metadata/taxonomy?flat=true`);
+	const indicators = await(await fetch(path)).json();
+
+	return {
+		indicators: indicators.filter(ind => !exclude.includes(ind.key))
+	};
+};
