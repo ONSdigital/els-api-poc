dbb4185d292e3f8f295c21dc923a0e006903a6fe
Ahmad Barclay <Ahmad.Barclay@ons.gov.uk>
2025-11-10 15:32:20 +0000

    return message when no data is available

--- src/lib/api/data/filterAllDatasets.js
+++ src/lib/api/data/filterAllDatasets.js
@@ -1,20 +1,32 @@
 import filterDataset from "./filterDataset.js";
 
-export default function filterAllDatasets(datasets, filters, params, format, singleIndicator = false) {
+export default function filterAllDatasets(
+  datasets,
+  filters,
+  params,
+  format,
+  singleIndicator = false
+) {
   const filtered = [];
   for (const cube of datasets) {
     const data = filterDataset(cube, filters, params, format, singleIndicator);
     if (data) filtered.push(data);
   }
-  console.log({datasets, filtered});
-  if (format === "csv") return filtered.map(f => f[1]);
+
+  if (format === "csv") return filtered.map((f) => f[1]);
+  if (!filtered?.length)
+    return { message: "Data not available for the selected filters." };
   if (["rows", "cols"].includes(format.slice(0, 4)))
-    return singleIndicator && filtered?.length === 1 ? filtered[0][1] : Object.fromEntries(filtered);
-  return singleIndicator ? filtered[0] : {
-    version: "2.0",
-    class: "collection",
-    label: "ONS Explore Local Statistics API response",
-    // updated: cube.updated,
-    link: {item: filtered}
-  };
+    return singleIndicator && filtered.length === 1
+      ? filtered[0][1]
+      : Object.fromEntries(filtered);
+  return singleIndicator
+    ? filtered[0]
+    : {
+        version: "2.0",
+        class: "collection",
+        label: "ONS Explore Local Statistics API response",
+        // updated: cube.updated,
+        link: { item: filtered },
+      };
 }
