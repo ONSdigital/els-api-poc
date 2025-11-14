8152578a1d635b701abe8cae27f08c138bc51ad6
Ahmad Barclay <Ahmad.Barclay@ons.gov.uk>
2025-11-14 11:32:17 +0000

    sort of fixed line chart

--- src/lib/viz/Line.svelte
+++ src/lib/viz/Line.svelte
@@ -1,5 +1,6 @@
 <script>
-  import { Plot, Line, Dot } from "svelteplot";
+  import { Line, Pointer, AxisX, AxisY } from "svelteplot";
+  import Plot from "svelteplot/core/Plot.svelte";
 
   let {
     data,
@@ -10,42 +11,75 @@
     selected = $bindable(),
   } = $props();
 
-  let keyedData = $derived.by(() => {
+  let _data = $derived.by(() => {
     const keyed = {};
-    for (const d of data){
-      if (!keyed[d[zKey]]) keyed[d[zKey]] = []
+    for (const d of data) {
+      if (!keyed[d[zKey]]) keyed[d[zKey]] = [];
       keyed[d[zKey]].push({ ...d, time: new Date(d.period.split("/")[0]) });
     }
-    return keyed;
+    return { keyed, array: Object.values(keyed).flat() };
   });
 
-  $inspect(keyedData)
+  let xDomain = $derived.by(() => {
+    const vals = _data.array.map((d) => d[xKey]);
+    return [Math.min(...vals), Math.max(...vals)];
+  });
+
+  let yDomain = $derived.by(() => {
+    const vals = _data.array.map((d) => d[yKey]);
+    return [Math.min(...vals), Math.max(...vals)];
+  });
+
+  function scaleX({ domain, plotWidth, plotOptions }) {
+    const { marginLeft, marginRight } = plotOptions;
+    const range = [marginLeft, plotWidth - marginLeft - marginRight];
+    const fn = (v) =>
+      ((v - domain[0]) / (domain[1] - domain[0])) * (range[1] - range[0]) +
+      range[0];
+    fn.range = () => range;
+    return fn;
+  }
+
+  function scaleY({ domain, plotHeight, plotOptions }) {
+    const { marginTop, marginBottom } = plotOptions;
+    const range = [plotHeight - marginTop - marginBottom, marginBottom];
+    const fn = (v) =>
+      ((v - domain[0]) / (domain[1] - domain[0])) * (range[1] - range[0]) +
+      range[0];
+    fn.range = () => range;
+    return fn;
+  }
 </script>
 
-<Plot x={{ grid: false }}>
+<Plot
+  x={{ domain: xDomain, scale: scaleX }}
+  y={{ domain: yDomain, scale: scaleY }}
+>
+  <AxisX />
+  <AxisY />
   <Line
-    data={Object.values(keyedData).flat()}
+    data={_data.array}
     x={xKey}
     y={yKey}
     z={zKey}
-    onpointerenter={(evt, d) => {
-      console.log("hovered series:", d.datum[zKey]);
-      hovered = d.datum[zKey];
-    }}
-    onpointerleave={() => (hovered = null)}
-    stroke= "grey"
+    stroke="grey"
     strokeWidth="1"
     pointerEvent="stroke"
   />
-    <Line class = "hovered-line"
-      data={hovered ? keyedData[hovered] : []}
-      x={xKey}
-      y={yKey}
-      z={zKey}
-      stroke="orange"
-      strokeWidth="3"
-      strokeOpacity="1"
-    />
+  <Pointer data={_data.array} x={xKey} y={yKey} z={zKey}>
+    {#snippet children({ data })}
+      <Line
+        class="hovered-line"
+        data={data[0] ? _data.keyed[data[0][zKey]] : []}
+        x={xKey}
+        y={yKey}
+        z={zKey}
+        stroke="orange"
+        strokeWidth="3"
+        strokeOpacity="1"
+      />
+    {/snippet}
+  </Pointer>
 </Plot>
 
 <style>
