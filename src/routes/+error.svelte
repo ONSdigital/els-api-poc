abc5de7d582d6890e200cd0e6b65c3359b10054f
Ahmad Barclay <ahmad.barclay@ons.gov.uk>
2026-01-20 13:50:40 +0000

    Add standard ONS-styled error pages

---
+++ src/routes/+error.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { page } from "$app/state";
+    import {
+        Header,
+        Main,
+        ErrorPage,
+        Footer,
+    } from "@onsvisual/svelte-components";
+</script>
+
+<Header />
+<Main>
+    <ErrorPage {page} />
+</Main>
+<Footer theme="dark" />
