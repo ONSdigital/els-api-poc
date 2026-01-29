4c929cc33bbf3262ccc308ee52dee98bc99cb978
Ahmad Barclay <ahmad.barclay@ons.gov.uk>
2026-01-29 10:18:07 +0000

    fix modals state bug

--- src/lib/components/modals/Modal.svelte
+++ src/lib/components/modals/Modal.svelte
@@ -1,13 +1,21 @@
 <script lang="ts">
-  import { Button } from "@onsvisual/svelte-components";
+	import { Button } from "@onsvisual/svelte-components";
 
-  let { title, label, icon = null, children, onConfirm, onCancel } = $props();
+	let {
+		title,
+		label,
+		icon = null,
+		children,
+		onOpen = () => null,
+		onConfirm = () => null,
+		onCancel = () => null
+	} = $props();
 
-  let id = $derived(title.toLowerCase().replaceAll(" ", "-"));
-  let dialog = $state();
-  
-  function clickOutsideToCancel(el) {
-    el.addEventListener('click', function (event) {
+	let id = $derived(title.toLowerCase().replaceAll(" ", "-"));
+	let dialog = $state();
+
+	function clickOutsideToCancel(el) {
+		el.addEventListener("click", function (event) {
 			const rect = el.getBoundingClientRect();
 			const isInDialog =
 				rect.top < event.clientY &&
@@ -16,45 +24,51 @@
 				event.clientX < rect.left + rect.width;
 			if (!isInDialog) {
 				el.close();
-        onCancel();
+				onCancel();
 			}
 		});
-  }
+	}
 </script>
 
-<Button variant="secondary" {icon} small on:click={() => dialog.showModal()}
-  >{label}</Button
+<Button
+	variant="secondary"
+	{icon}
+	small
+	on:click={() => {
+		onOpen();
+		dialog.showModal();
+	}}>{label}</Button
 >
 
 <dialog aria-labelledby={id} bind:this={dialog} use:clickOutsideToCancel>
-  <h1 {id} tabindex="-1">{title}</h1>
-  <div class="modal-contents">
-    {@render children()}
-  </div>
-  <Button
-    small
-    on:click={() => {
-      onConfirm();
-      dialog.close();
-    }}>Confirm changes</Button
-  >
-  <Button
-    variant="secondary"
-    small
-    on:click={() => {
-      dialog.close();
-      onCancel();
-    }}>Cancel your selection</Button
-  >
+	<h1 {id} tabindex="-1">{title}</h1>
+	<div class="modal-contents">
+		{@render children()}
+	</div>
+	<Button
+		small
+		on:click={() => {
+			onConfirm();
+			dialog.close();
+		}}>Confirm changes</Button
+	>
+	<Button
+		variant="secondary"
+		small
+		on:click={() => {
+			dialog.close();
+			onCancel();
+		}}>Cancel your selection</Button
+	>
 </dialog>
 
 <style>
-  dialog {
-    width: 760px;
-    max-width: calc(100% - 2rem);
-    overflow: visible;
-  }
-  .modal-contents {
-    margin-bottom: 2rem;
-  }
+	dialog {
+		width: 760px;
+		max-width: calc(100% - 2rem);
+		overflow: visible;
+	}
+	.modal-contents {
+		margin-bottom: 2rem;
+	}
 </style>
